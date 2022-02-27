3535d88520425013e8280c75654c9d0d0078328c
Phuwarin 🥱 <phuwarin.21@gmail.com>
2022-02-27 16:57:22 +0700

    Add method in Repository to use in Service. (#5)

--- src/main/java/com/example/assignment1/repositories/UserBasketRepository.java
+++ src/main/java/com/example/assignment1/repositories/UserBasketRepository.java
@@ -3,6 +3,10 @@ package com.example.assignment1.repositories;
 import com.example.assignment1.database.UserBasket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserBasketRepository extends JpaRepository<UserBasket, Long> {
+
+    List<UserBasket> findByUserId(long userId);
 
 }
